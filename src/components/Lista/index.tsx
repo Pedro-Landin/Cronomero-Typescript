401866e560025c77f231b3b35166150d29b3045d
Pedro landim <pedrolandin@pandanjo.com>
2023-07-03 18:35:01 -0300

    Selecionando um item

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -2,20 +2,26 @@ import { ITarefa } from '../../types/tarefa';
 import style from './Lista.module.scss';
 import Item from './item';
 
-function Lista({ tarefas }: {tarefas: ITarefa[]}){
+interface Props {
+  tarefas: ITarefa[],
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+function Lista({ tarefas, selecionaTarefa }: Props){
     return(
         <aside className={style.listaTarefas}>
             <h2>Estudos do dia</h2>
             <ul>
-              {tarefas.map((item, index) => (
-                <Item 
-                    key={index}
-/*                     id={item.id}
+              {tarefas.map(item => (
+                <Item
+                    key={item.id} 
+/*                  id={item.id}
                     selecionado={item.selecionado}
                     tarefa={item.tarefa}
                     completado={item.completado}
                     tempo={item.tempo} */
                     {...item}
+                    selecionaTarefa={selecionaTarefa}
                 />
               ))}
             </ul>
